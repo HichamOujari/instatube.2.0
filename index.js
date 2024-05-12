70ad5a6f115e0afa4770bbfdecbce7f11fad2826
hichamoujari <hicham.oujari@izi.net>
2024-05-12 18:41:14 +0100

    message

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post("/merge", (req, res) => {
           const reelsDuration = Math.min(shorterDuration, 60);
 
           // Trim longer file to match shorter duration and adapt to Instagram Reels specs
-          const ffmpegCommand = `ffmpeg -i ${tempAudioFile} -i ${tempVideoFile} -t ${reelsDuration} -vf "scale=720:1280:force_original_aspect_ratio=decrease,pad=720:1280:-1:-1,setsar=1" -c:v libx264 -b:v 1M -c:a aac -strict experimental ${outputFilePath}`;
+          const ffmpegCommand = `ffmpeg -i ${tempAudioFile} -i ${tempVideoFile} -t ${reelsDuration} -vf "scale=720:1280:force_original_aspect_ratio=decrease,pad=720:1280:-1:-1,setsar=1" -c:v libx264 -b:v 1M -c:a aac -strict experimental ${outputFilePath} -preset veryfast`;
 
           // Merge audio and video using ffmpeg
           exec(ffmpegCommand, (error, stdout, stderr) => {
@@ -105,5 +105,5 @@ const port = process.env.prod ?? 5501;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-  localtunnel({ port: parseInt(port), subdomain: "instatube-generator" });
+  //localtunnel({ port: parseInt(port), subdomain: "instatube-generator" });
 });
